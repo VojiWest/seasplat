fixed gradient tracker

scene/gaussian_model.py
@@ -54,6 +54,7 @@ class GaussianModel:
         self.xyz_gradient_accum = torch.empty(0)
         self.denom = torch.empty(0)
         self.xyz_gradient_individual = torch.empty(0)
+        self.img_idxs_grads_stored = torch.empty(0)
         self.optimizer = None
         self.percent_dense = 0
         self.spatial_lr_scale = 0
@@ -487,16 +488,20 @@ class GaussianModel:
 
     def add_grad(self, viewspace_point_tensor, visibility_filter, image_idx, num_images, method="split"):
         if self.xyz_gradient_individual.shape[0] < 2: 
+            self.img_idxs_grads_stored = torch.zeros(num_images)
             if method == "norm": # If empty then initialize to shape num_gaussians x num_images
-                self.xyz_gradient_individual = torch.zeros((self.get_xyz.shape[0], num_images), device="cuda")
+                self.xyz_gradient_individual = torch.full((self.get_xyz.shape[0], num_images), fill_value=float('nan'), dtype=torch.float32, device="cuda")
             if method == "split":
-                self.xyz_gradient_individual = torch.zeros((self.get_xyz.shape[0], num_images, 2), device="cuda")
+                self.xyz_gradient_individual = torch.full((self.get_xyz.shape[0], num_images, 2), fill_value=float('nan'), dtype=torch.float32, device="cuda")
 
         if method == "norm":
             self.xyz_gradient_individual[visibility_filter, image_idx] = torch.norm(viewspace_point_tensor.grad[visibility_filter,:2], dim=-1)
         if method == "split":
             self.xyz_gradient_individual[visibility_filter, image_idx] = viewspace_point_tensor.grad[visibility_filter,:2]
 
+        self.img_idxs_grads_stored[image_idx] = 1
+
     def reset_grad_tracking(self):
         self.xyz_gradient_individual = torch.empty(0)
+        self.img_idxs_grads_stored = torch.empty(0)
 

train_new.py
@@ -210,7 +210,7 @@ def training(model_params, opt_params, pipe_params, testing_iterations, saving_i
                 # do not use learned background; rely on backscatter to hopefully fill this in
                 image = rendered_image
                 if not done_binf_init_with_bg:
-                    print(f"[{iteration} updated bs_model B_inf with learnred_bg parameters {torch.sigmoid(learned_bg)}")
+                    print(f"[{iteration}] updated bs_model B_inf with learnred_bg parameters {torch.sigmoid(learned_bg)}")
                     bs_model.B_inf = torch.nn.Parameter(torch.clone(learned_bg.data).reshape(3, 1, 1)).cuda()
                     bs_optimizer = torch.optim.Adam(bs_model.parameters(), lr=opt_params.bs_at_lr)
                     done_binf_init_with_bg = True
@@ -521,8 +521,9 @@ def training(model_params, opt_params, pipe_params, testing_iterations, saving_i
             # Add gradient for tracking variance
             gaussians.add_grad(viewspace_point_tensor, visibility_filter, img_idx, len(scene.getTrainCameras()))
 
-            print("Num Train Cameras: ", len(scene.getTrainCameras()), "Num Left in Viewpoint_stack: ", len(viewpoint_stack))
-            if abs(10000 - (iteration % 10000)) <= len(scene.getTrainCameras()) and not viewpoint_stack:
+            # print("Num Train Cameras: ", len(scene.getTrainCameras()), "Num added to grad tracker: ", torch.count_nonzero(gaussians.img_idxs_grads_stored))
+            # TODO Fix when this if statement is triggered (specifically the first part)
+            if abs(10000 - (iteration % 10000)) <= 5 * len(scene.getTrainCameras()) and torch.count_nonzero(gaussians.img_idxs_grads_stored) == (len(scene.getTrainCameras())):
                 print("It's Filtering Time!")
                 if opt_params.do_seathru and iteration > opt_params.seathru_from_iter:
                     evaluate_gaussian_filtering(tb_writer, opt_params, iteration, testing_iterations, scene, render, (pipe_params, background),
@@ -545,7 +546,7 @@ def training(model_params, opt_params, pipe_params, testing_iterations, saving_i
                 
             if iteration <= opt_params.densify_until_iter and iteration % opt_params.densification_interval == 0:
                     gaussians.reset_grad_tracking()
-            elif iteration > opt_params.densify_until_iter and not viewpoint_stack: # Once cycled through all images, reset
+            elif iteration > opt_params.densify_until_iter and torch.count_nonzero(gaussians.img_idxs_grads_stored) == (len(scene.getTrainCameras())): # Once cycled through all images, reset
                     gaussians.reset_grad_tracking()
 
             # Densification

utils/filter_utils.py
@@ -55,9 +55,9 @@ def calculate_spread(gaussian_non_zero, method):
             sd = torch.std(gaussian_non_zero, dim=0)
             spread = sd
     elif gaussian_non_zero.numel() > 0:
-        spread = 0
+        spread = torch.tensor(0.0, device=gaussian_non_zero.device, dtype=gaussian_non_zero.dtype)
     else:
-        spread = -1
+        spread = torch.tensor(-1.0, device=gaussian_non_zero.device, dtype=gaussian_non_zero.dtype)
 
     return spread
 
@@ -68,20 +68,23 @@ def get_inter_view_gradient_variance(gradients, method='var', model_path="", ite
         if gaussian_grads.ndim == 2:
             g1 = gaussian_grads[:,0]
             g2 = gaussian_grads[:,1]
-            
-            g1_non_zero = g1[g1 > 0]
-            g2_non_zero = g2[g2 > 0]
 
-            numels.append(g1_non_zero.numel())
-            numels.append(g2_non_zero.numel())
+            mask_g1 = ~torch.isnan(g1)
+            mask_g2 = ~torch.isnan(g2)
 
-            v1 = calculate_spread(g1_non_zero, method=method)
-            v2 = calculate_spread(g2_non_zero, method=method)
+            g1_valid = g1[mask_g1]
+            g2_valid = g2[mask_g2]
+
+            numels.append(g1_valid.numel())
+            numels.append(g2_valid.numel())
+
+            v1 = calculate_spread(g1_valid, method=method)
+            v2 = calculate_spread(g2_valid, method=method)
 
             spreads[idx] = torch.mean(torch.stack([v1, v2]))
         
         elif gaussian_grads.ndim == 1:
-            mask = gaussian_grads > 0
+            mask = ~torch.isnan(gaussian_grads)
             gaussian_non_zero = gaussian_grads[mask]
 
             numels.append(gaussian_non_zero.numel())